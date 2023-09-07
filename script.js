6b3f9f435a1bda13124f698866d384d5921fdd07
Dusin-RS <omirzak.d@phystech.edu>
2023-09-08 00:29:52 +0300

    Fixed date format added comments

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const nextMonthButton = document.getElementById("next-month");
 
 let selectedDate = new Date();
 
-function updateCalendar() {
+function updateCalendar(day_in=1) {
+	// Updates calendarHtml in every action
     const selectedMonth = monthSelector.selectedIndex;
     const selectedYear = parseInt(yearSelector.value);
     const daysInMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
@@ -20,7 +21,7 @@ function updateCalendar() {
     let calendarHTML = '<table>';
     calendarHTML += '<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>';
     
-    let day = 1;
+    let day = day_in;
     for (let i = 0; i < 6; i++) {
         calendarHTML += '<tr>';
         for (let j = 0; j < 7; j++) {
@@ -33,7 +34,7 @@ function updateCalendar() {
                 const isCurrentYear = date.getFullYear() === currentDate.getFullYear();
                 const isToday = isCurrentMonth && isCurrentYear && day === currentDate.getDate();
                 const isSelected = selectedDate.toDateString() === date.toDateString();
-
+				// Depended on className the style of cell will be changing for selected date and non-selected
                 const classNames = [];
                 if (isToday) classNames.push("today");
                 if (isSelected) classNames.push("selected");
@@ -49,19 +50,21 @@ function updateCalendar() {
     }
     calendarHTML += '</table>';
     calendar.innerHTML = calendarHTML;
-
+	
+	// Set for each sell EventListener which will change selected date depended on click
     const calendarDays = calendar.querySelectorAll("td");
     calendarDays.forEach(dayElement => {
         dayElement.addEventListener("click", () => {
             const day = parseInt(dayElement.textContent);
             selectedDate = new Date(selectedYear, selectedMonth, day);
             updateCalendar();
-            selectedDateInput.value = selectedDate.toLocaleDateString();
+            selectedDateInput.value = selectedDate.toLocaleDateString("en-US");
         });
     });
 }
 
 months.forEach((month, index) => {
+	// Month list
     const option = document.createElement("option");
     option.value = index;
     option.textContent = month;
@@ -69,6 +72,7 @@ months.forEach((month, index) => {
 });
 
 years.forEach(year => {
+	// Year list
     const option = document.createElement("option");
     option.value = year;
     option.textContent = year;
@@ -79,6 +83,7 @@ monthSelector.selectedIndex = selectedDate.getMonth();
 yearSelector.value = selectedDate.getFullYear();
 
 prevMonthButton.addEventListener("click", () => {
+	//EventListener for prevMonthButton which will decrement the month number
     selectedDate.setMonth(selectedDate.getMonth() - 1);
     if (selectedDate.getMonth() < 0) {
         selectedDate.setMonth(11);
@@ -90,16 +95,17 @@ prevMonthButton.addEventListener("click", () => {
 });
 
 nextMonthButton.addEventListener("click", () => {
+	//EventListener for nextMonthButton which will increment the month number
     selectedDate.setMonth(selectedDate.getMonth() + 1);
     if (selectedDate.getMonth() > 11) {
         selectedDate.setMonth(0);
         selectedDate.setFullYear(selectedDate.getFullYear() + 1);
+
     }
     updateCalendar();
     monthSelector.selectedIndex = selectedDate.getMonth();
     yearSelector.value = selectedDate.getFullYear();
 });
-
 function isValidDate(date) {
     return date instanceof Date && !isNaN(date);
 }
@@ -124,4 +130,4 @@ selectedDateInput.addEventListener("focus", () => {
 });
 
 updateCalendar();
-selectedDateInput.value = selectedDate.toLocaleDateString();
+selectedDateInput.value = selectedDate.toLocaleDateString("en-US");
