4be6897fb5cff5b75f2cafcfed14554215b16ab7
Dusin-RS <omirzak.d@phystech.edu>
2023-09-07 23:50:56 +0300

    Fixed bugs with Invalid date, improved design, and added comments

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -12,43 +12,53 @@ const App: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [year, setYear] = useState<number>(selectedDate.getFullYear());
   const [month, setMonth] = useState<number>(selectedDate.getMonth());
-  const [inputDate, setInputDate] = useState<string>(selectedDate.toLocaleDateString());
+  const [monthDay, setMonthDay] = useState<number>(selectedDate.getDate());
+  const [inputDate, setInputDate] = useState<string>(selectedDate.toLocaleDateString("en-US"));
   const [calendarHTML, setCalendarHTML] = useState<string>('');
+  const [isActive, setActive] = useState<boolean>(false);
+  
   const selectedMonth: number = month;
   const selectedYear: number = year;
-  const [isActive, setActive] = useState<boolean>(false);
   const daysInMonth: number = new Date(selectedYear, selectedMonth + 1, 0).getDate();
   const firstDayOfMonth: number = new Date(selectedYear, selectedMonth, 1).getDay();
 
   const handleDateClick = (day: number) => {
     const newSelectedDate: Date = new Date(year, month, day);
     setSelectedDate(newSelectedDate);
-    setInputDate(newSelectedDate.toLocaleDateString());
+    setInputDate(newSelectedDate.toLocaleDateString("en-US"));
   };
-
+  
   const handleMonthChange = (increment: number) => {
+	// Change current month by negative or positive 1 depends on button
     const newMonth: number = month + increment;
     if (newMonth < 0) {
+	  // Edge case, when we moving from january to december
       setMonth(11);
       setYear(year - 1);
     } else if (newMonth > 11) {
+	  // Edge case, when we moving from december to january
       setMonth(0);
       setYear(year + 1);
     } else {
+	// All other cases
       setMonth(newMonth);
     }
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Set current Date from input field
     setInputDate(e.target.value);
   };
 
   const handleInputBlur = () => {
+    // Check input value for valid values
     const newDate: Date = new Date(inputDate);
-    const date = moment(inputDate, 'MM/DD/YYYY', true); // Validate as MM/DD/YYYY
+    const date = moment(inputDate, 'MM/DD/YYYY'); 
+	// Create moment for checking is date valid
     if (date.isValid()) {
       setYear(newDate.getFullYear());
       setMonth(newDate.getMonth());
+	  setMonthDay(newDate.getDate());
       setSelectedDate(newDate);
     } else {
       setInputDate('Invalid Date');
@@ -58,7 +68,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="header">
-        <button onClick={() => handleMonthChange(-1)}>&#8249;</button>
+        <button className='prev-month' onClick={() => handleMonthChange(-1)}>&#8249;</button>
         <select value={month} onChange={(e) => setMonth(parseInt(e.target.value))}>
           {months.map((monthName, index) => (
             <option key={index} value={index}>{monthName}</option>
@@ -69,7 +79,7 @@ const App: React.FC = () => {
             <option key={i} value={year - 5 + i}>{year - 5 + i}</option>
           ))}
         </select>
-        <button onClick={() => handleMonthChange(1)}>&#8250;</button>
+        <button className='next-month' onClick={() => handleMonthChange(1)}>&#8250;</button>
       </div>
       <table>
         <thead>
